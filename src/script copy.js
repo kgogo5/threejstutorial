d1d3e040662af58f39e51d98e0800f054ca43bad
Steve <kgogo9@gmail.com>
2021-06-29 15:41:54 +0900

    라이트 두개 적용한 회전하는 구

---
+++ src/script copy.js
@@ -0,0 +1,135 @@
+import "./style.css"
+import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
+import * as dat from "dat.gui"
+import { FlatShading } from "three"
+
+//Loading
+const textureLoader = new THREE.TextureLoader()
+// const normalTexture = textureLoader.load("/textures/rocks-autogen.png") // 텍스쳐 파일 지정
+const normalTexture = textureLoader.load("/textures/height2.png") // 텍스쳐 파일 지정
+
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl")
+
+// Scene
+const scene = new THREE.Scene()
+
+// Objects
+// const geometry = new THREE.TorusGeometry(0.8, 0.05, 16, 100)
+const geometry = new THREE.SphereBufferGeometry(0.5, 64, 64)
+
+// Materials
+// const material = new THREE.MeshBasicMaterial()
+const material = new THREE.MeshStandardMaterial()
+material.metalness = 0.7
+material.roughness = 0.2
+// material.flatShading = true
+// material.vertexColors = true
+// material.fog = true
+material.normalMap = normalTexture;
+
+material.color = new THREE.Color(0x292929)
+
+// Mesh
+const sphere = new THREE.Mesh(geometry, material)
+scene.add(sphere)
+
+// Lights
+
+const pointLight = new THREE.PointLight(0xffffff, 0.1)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
+
+const pointLight2 = new THREE.PointLight(0xff0000, 2)
+// pointLight.position.x = 2
+// pointLight.position.y = 3
+// pointLight.position.z = 4
+pointLight2.position.set(1,1,1)
+pointLight2.intensity = 1
+
+scene.add(pointLight)
+
+gui.add(pointLight2.position, 'y')
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+)
+camera.position.x = 0
+camera.position.y = 0
+camera.position.z = 2
+scene.add(camera)
+
+// Controls
+// const controls = new OrbitControls(camera, canvas)
+// controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  alpha: true,
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+
+const clock = new THREE.Clock()
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+
+  // Update objects
+  sphere.rotation.y = 0.5 * elapsedTime
+  // sphere.rotation.x = 1.2 * elapsedTime
+  // sphere.rotation.z = 2.2 * elapsedTime
+
+  // Update Orbital Controls
+  // controls.update()
+
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
+}
+
+tick()
--- src/script.js
+++ src/script.js
@@ -7,27 +7,27 @@ import { FlatShading } from "three";
 //loading
 const textureLoader = new THREE.TextureLoader();
 
-const normalTexture = textureLoader.load("/textures/rocks-autogen.png");
+const normalTexture = textureLoader.load('/textures/height2.png');
 
 // Debug
 const gui = new dat.GUI();
 
 // Canvas
-const canvas = document.querySelector("canvas.webgl");
+const canvas = document.querySelector('canvas.webgl');
 
 // Scene
 const scene = new THREE.Scene();
 
 // Objects
-// const geometry = new THREE.TorusGeometry(0.8, 0.05, 16, 100);
+// const geometry = new THREE.TorusGeometry(0.8, 0.1, 16, 100);
 const geometry = new THREE.SphereBufferGeometry(0.5, 64, 64);
 
 // Materials
 
-const material = new THREE.MeshStandardMaterial();
-material.color = new THREE.Color(0xfffb29);
-material.metalness = 0.1;
-material.roughness = 0.6;
+const material = new THREE.MeshStandardMaterial()
+material.color = new THREE.Color(0x292929)
+material.metalness = 0.7
+material.roughness = 0.2
 // material.flatShading = true;
 // material.vertexColors = true;
 // material.fog = true;
@@ -39,11 +39,62 @@ scene.add(sphere);
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xff6ed9, 0.8);
+const pointLight = new THREE.PointLight(0xffffff, 0.1)
 pointLight.position.x = 2;
 pointLight.position.y = 3;
 pointLight.position.z = 4;
 scene.add(pointLight);
+
+// Lights2
+const pointLight2 = new THREE.PointLight(0x93361, 2)
+pointLight2.position.set(2.6, 2, -1.52)
+pointLight2.intensity = 10
+
+scene.add(pointLight2)
+
+const light1 = gui.addFolder('Light 1')
+
+light1.add(pointLight2.position, 'y').min(-3).max(3).step(0.01)
+light1.add(pointLight2.position, 'x').min(-6).max(6).step(0.01)
+light1.add(pointLight2.position, 'z').min(-3).max(3).step(0.01)
+light1.add(pointLight2, 'intensity').min(0).max(10).step(0.01)
+
+const light1Color = {
+  color : 0xff0000
+}
+
+light1.addColor(light1Color, 'color')
+  .onChange(()=>{
+    pointLight2.color.set(light1Color.color)
+  })
+
+// const pointLightHelper = new THREE.PointLightHelper(pointLight2, 0.5)
+// scene.add(pointLightHelper)
+
+// Lights3
+const pointLight3 = new THREE.PointLight(0x593896, 2)
+pointLight3.position.set(-0.66, -0.56, 0.14)
+pointLight3.intensity = 10
+
+scene.add(pointLight3)
+const light2 = gui.addFolder('Light 2')
+
+light2.add(pointLight3.position, 'y').min(-3).max(3).step(0.01)
+light2.add(pointLight3.position, 'x').min(-6).max(6).step(0.01)
+light2.add(pointLight3.position, 'z').min(-3).max(3).step(0.01)
+light2.add(pointLight3, 'intensity').min(0).max(10).step(0.01)
+
+const light2Color = {
+  color : 0xff0000
+}
+
+light2.addColor(light2Color, 'color')
+  .onChange(()=>{
+    pointLight3.color.set(light2Color.color)
+  })
+
+// const pointLightHelper2 = new THREE.PointLightHelper(pointLight3, 0.5)
+// scene.add(pointLightHelper2)
 
 /**
  * Sizes
@@ -106,9 +157,9 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
   // Update objects
-  sphere.rotation.y = 0.2 * elapsedTime;
-  sphere.rotation.x = 1.2 * elapsedTime;
-  sphere.rotation.z = 2.2 * elapsedTime;
+  sphere.rotation.y = 0.5 * elapsedTime;
+  sphere.rotation.x = 0.2 * elapsedTime;
+  sphere.rotation.z = 0.4 * elapsedTime;
 
   // Update Orbital Controls
   // controls.update()
