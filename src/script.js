0f3be27a05c26935ff52bf979e9168882e88f6ef
Steve <kgogo9@gmail.com>
2021-09-16 04:28:28 +0900

    gsap 강력한 애니메이션 플러그인 설치 활용 scroll Raycaster 완성

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "dat.gui";
+import gsap from "gsap";
 
 // Texture Loader
 const textureLoader = new THREE.TextureLoader();
@@ -25,10 +26,16 @@ for (let i = 0; i <= 5; i++) {
 
   const img = new THREE.Mesh(geometry, material);
 
-  img.position.set(1, i * 1.8);
+  img.position.set(Math.random() + 0.3, i * -1.8);
 
   scene.add(img);
 }
+
+let objs = [];
+
+scene.traverse((object) => {
+  if (object.isMesh) objs.push(object);
+});
 
 // Lights
 
@@ -90,9 +97,29 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
+// Mouse
+
+window.addEventListener("wheel", onMouseWheel);
+
+let y = 0;
+let position = 0;
+
+function onMouseWheel(e) {
+  y = e.deltaY * 0.0007;
+}
+
+const mouse = new THREE.Vector2();
+
+window.addEventListener("mousemove", (e) => {
+  mouse.x = (e.clientX / sizes.width) * 2 - 1;
+  mouse.y = -(e.clientY / sizes.height) * 2 + 1;
+});
+
 /**
  * Animate
  */
+
+const raycaster = new THREE.Raycaster();
 
 const clock = new THREE.Clock();
 
@@ -100,6 +127,29 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
   // Update objects
+
+  position += y;
+  y *= 0.9;
+
+  // Raycaster
+  raycaster.setFromCamera(mouse, camera);
+  const intersects = raycaster.intersectObjects(objs);
+
+  for (const intersect of intersects) {
+    gsap.to(intersect.object.scale, { x: 1.7, y: 1.7 });
+    gsap.to(intersect.object.rotation, { y: -0.5 });
+    gsap.to(intersect.object.position, { z: -0.9 });
+  }
+
+  for (const object of objs) {
+    if (!intersects.find((intersect) => intersect.object === object)) {
+      gsap.to(object.scale, { x: 1, y: 1 });
+      gsap.to(object.rotation, { y: 0 });
+      gsap.to(object.position, { z: 0 });
+    }
+  }
+
+  camera.position.y = -position;
 
   // Update Orbital Controls
   // controls.update()
